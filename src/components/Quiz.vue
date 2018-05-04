957ceb8031418f4ad307748ffc0ee7c019e0733b
Jon Barson <jbarson@infotech.com>
2018-05-04 13:40:08 -0400

    adding styles to the title

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -3,7 +3,23 @@
   <div id="container">
     <button @click='resetPage'>Reset</button>
   <quiz-title  v-if='titleScreen'/>
-  <button @click='startQuiz' v-if='titleScreen'>Get Started</button>
+  <div @click='startQuiz' class="get-started">
+    <svg width="169px" height="60px" viewBox="0 0 169 60" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 49.3 (51167) - http://www.bohemiancoding.com/sketch -->
+      <desc>Created with Sketch.</desc>
+      <defs></defs>
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Artboard-Copy-7" transform="translate(-1210.000000, -649.000000)">
+                <g id="Group" transform="translate(1210.000000, 649.000000)">
+                    <rect id="bg-copy-3" fill="#2576B7" x="0" y="0" width="169" height="60"></rect>
+                    <text id="Get-Started" font-family="Exo-Bold, Exo" font-size="16" font-weight="bold" fill="#FFFFFF">
+                        <tspan x="35.196" y="36">GET STARTED</tspan>
+                    </text>
+                </g>
+            </g>
+        </g>
+    </svg>
+    </div>
   <div v-if='!titleScreen'>
     <div v-if="!calced">
       <div v-for="question in sectionedQuestions" v-bind:key='question.id' class="result-copy-container">
@@ -410,5 +426,8 @@ export default {
 }
 .blueprint {
   padding: 30px;
+}
+.get-started {
+  float: right;
 }
 </style>
--- src/components/QuizTitle.vue
+++ src/components/QuizTitle.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <h2>- Info-Tech Research Group</h2>
+  <div class="title-container">
+    <img class="nav-logo" src="//www.infotech.com/assets/logos/Info-Tech-Research-Group-logo-9f60c2aabd41ec5f65952641ee795ea5202bfa0b3009ab614d32518d4d3ba55f.svg">
+    <h2>---- INFO-TECH RESEARCH GROUP</h2>
+    <h1 class="title-header">IT PERSONALITY TEST</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+    <p style="float: right; display: block;">some more lorem ipsum</p>
   </div>
 </template>
 
@@ -10,6 +14,20 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+  .nav-logo {
+    float: right;
+    height: 50px;
+  }
+  .title-container {
+  margin-top: 10px;
+  .title-header {
+    font-family: Montserrat;
+    font-size: 100px;
+    color: #373636;
+    line-height: 46px;
+    margin: 10px 0 0;
+  }
+  }
 
 </style>
