0706e954cab216978cd9a3d1b4a22c155b6d201f
Jon Barson <jbarson@infotech.com>
2018-05-04 09:49:59 -0400

    new version

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Title from '@/components/Title'
-import Quiz1 from '@/components/Quiz1'
-import Quiz2 from '@/components/Quiz2'
-import Quiz3 from '@/components/Quiz3'
+import Quiz from '@/components/Quiz'
+import Outcome from '@/components/Outcome'
 
 
 
@@ -13,8 +12,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {path: '/', component: Title},
-    {path: '/quiz1', component: Quiz1},
-    {path: '/quiz2', component: Quiz2},
-    {path: '/quiz3', component: Quiz3},
+    {path: '/quiz', component: Quiz},
+    {path: '/outcome', component: Outcome},
   ]
 })
